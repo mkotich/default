c74e7ab11605c989970ce3c41e675c58c90470c6
mkotich <49791448+mkotich@users.noreply.github.com>
2020-01-28 16:17:07 -0800

    Removing stuff

--- One_Link_Build_Tools/software/pbis_configuration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/opt/pbis/bin/config UserDomainPrefix "BGCDEV"
-/opt/pbis/bin/config AssumeDefaultDomain "true"
-/opt/pbis/bin/config HomeDirTemplate "%H/%U"
-/opt/pbis/bin/config RemoteHomeDirTemplate "%H/%U"
-/opt/pbis/bin/config HomeDirUmask "077"
-/opt/pbis/bin/config LoginShellTemplate "/bin/bash"
-/opt/pbis/bin/config Local_HomeDirTemplate "%H/%U"
-/opt/pbis/bin/config Local_HomeDirUmask "077"
